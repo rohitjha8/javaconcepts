4bc8c4e9d4f9fa8ac3485d4fdf84ab4ff1f56ccf
Rohit <kumarrohi88@gmail.com>
2024-08-08 19:00:37 +0530

    1st class commited

--- Jva_concepts/src/com/rohit/introduction/Main.java
+++ Jva_concepts/src/com/rohit/introduction/Main.java
@@ -6,9 +6,28 @@ public class Main {
 //      String [] names=new String[5];
 //      Student kunal;// this refernce varible kunal is of type student which our owrn datatype
 
-        Student s1=new Student();
-        s1.rollno=45;
+        Student s1=new Student("rohit",23,78.5f);
+//        s1.rollno=45;
+//        s1.marks=5.5f;
+//        s1.name="rohit";
+//        s1.changename("jha");
+//        s1.greeting();
+
         System.out.println(s1.rollno);
+        System.out.println(s1.marks);
+        System.out.println(s1.name);
+
+
+        Student one = new Student();
+        Student two=one;
+        one.name="something0";
+        System.out.println(two.name);
+
+        Student random=new Student(s1);
+        System.out.println(random.name);
+
+        Student random2=new Student();
+
 
 
     }}
@@ -16,4 +35,32 @@ public class Main {
         String name;
         int rollno;
         float marks;
+        void greeting(){
+            System.out.println("hello my name is " + this.name);
+        }
+        void changename(String newname){
+            name=newname;
+            //this.name=name;
+        }
+        Student (Student other){
+            this.name=other.name;
+        }
+        Student(){
+//            this.rollno=45;
+//            this.marks=5.5f;
+//            this.name="rohit";
+            //this is how you call a constructor from another constructor
+            //internaly new Student();
+            this("randodm2",34,98.5f);
+        }
+        //when you call a constructor with 3 values it will call this or the above one wiht 0 vlaue
+        //Student arpit=new Student("rhit",7,900)
+        //here this will be replaced by arpit
+
+        Student(String naam,int roll,float mark){
+            rollno=roll;
+            //this.rollno=rollno;// this will work
+            name=naam;
+            marks=mark;
+        }
     }
---
+++ Jva_concepts/src/com/rohit/introduction/WrapperExample.java
@@ -0,0 +1,34 @@
+package com.rohit.introduction;
+
+public class WrapperExample {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        Integer num=4;
+//        Integer.//this is a way of converting primitive datatype to object which will have mor functionalities```
+        swap(a,b);
+        System.out.println(a+" "+b);
+        final int bonus=3;
+        //bonus=9;//cant modify because its a final variable
+    final A rohit = new A("rohitjah");
+    this.name="ohti";
+
+    }
+    static void  swap(int a, int b){
+        int temp=a;
+        a=b;
+        b=temp;
+        class A{
+            final int num=70;
+            String name;
+            public A(String name){
+                this.name=name;
+            }
+    }
+//    class Rohit{
+//        //final int num;//
+//        //final variable should be initialized
+//    }
+
+}
+}
