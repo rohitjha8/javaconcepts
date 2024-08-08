2199cf002c8ee9c214f22ce5ac3f1c933ff7a1a4
Rohit <kumarrohi88@gmail.com>
2024-08-08 12:59:27 +0530

    rohit

--- Jva_concepts/src/com/rohit/introduction/Main.java
+++ Jva_concepts/src/com/rohit/introduction/Main.java
@@ -1,2 +1,19 @@
-package com.rohit.introduction;public class Main {
-}
+package com.rohit.introduction;
+
+public class Main {
+    public   static void  main(String[] args) {
+//      int [] num=new int[5];
+//      String [] names=new String[5];
+//      Student kunal;// this refernce varible kunal is of type student which our owrn datatype
+
+        Student s1=new Student();
+        s1.rollno=45;
+        System.out.println(s1.rollno);
+
+
+    }}
+    class Student{
+        String name;
+        int rollno;d
+        float marks;
+    }
